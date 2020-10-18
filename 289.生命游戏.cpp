1767f2c99cd3064653c8f65ddeac105faefcf3ec
PureDeep <542057950@qq.com>
2020-10-18 16:41:03 +0800

    2020-10-18-1

---
+++ 289.生命游戏.cpp
@@ -0,0 +1,23 @@
+/*
+ * @Author: PureDeep
+ * @Date: 2020-10-13 17:43:17
+ * @LastEditors: PureDeep
+ * @LastEditTime: 2020-10-13 17:46:59
+ * @FilePath: \LeetCode\289.生命游戏.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=289 lang=cpp
+ *
+ * [289] 生命游戏
+ */
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    void gameOfLife(vector<vector<int>> &board)
+    {
+    }
+};
+// @lc code=end
